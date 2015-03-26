10b9fcea6b4b5678a4cafe08fd437d3ec4e572d5
Lars <K3LARRA@HOTMAIL.COM>
2015-03-26 10:10:33 +0100

    Initial commit

---
+++ app/src/androidTest/java/se/mah/k3/pfi3_2015_k3larra_h/ApplicationTest.java
@@ -0,0 +1,13 @@
+package se.mah.k3.pfi3_2015_k3larra_h;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
